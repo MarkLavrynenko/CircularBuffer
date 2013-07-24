695df4401fd4c372284ef8abc676359707c67523
Mark Lavrynenko <MarkLavrinenko@gmail.com>
2013-07-24 18:09:45 +0300

    recommit with all projects

---
+++ CircularBuffer.Tests/CircularBufferTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CircularBuffer;
+
+namespace CircularBuffer.Tests
+{
+    [TestClass]
+    public class CircularBufferTests
+    {
+        private static Random rand;
+
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+        }
+
+        [TestMethod]
+        public void FillBuffer()
+        {
+            CircularBuffer<int> buf = new CircularBuffer<int>(15);
+            Assert.IsTrue(buf.Capacity == 15);
+        }
+
+        [TestMethod]
+        public void IterateBuffer()
+        {
+            Assert.IsTrue(false, "Not implemented");
+        }
+
+        [TestMethod]
+        public void CleanBuffer()
+        {
+            Assert.IsTrue(false, "Not implemented");
+        }
+
+        [TestMethod]
+        public void OverFlowBuffer()
+        {
+            Assert.IsTrue(false, "Not implemented");
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.IsTrue(false, "Not implemented");
+        }
+    }
+}
