8965eeba604d1a9358cd049215b6275595f77be2
Mark Lavrynenko <MarkLavrinenko@gmail.com>
2013-07-30 22:34:05 +0300

    add constructor test, fixed minor ReSharper warnings

--- CircularBuffer.Tests/CircularBufferTests.cs
+++ CircularBuffer.Tests/CircularBufferTests.cs
@@ -5,14 +5,13 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CircularBuffer;
 
 namespace CircularBuffer.Tests
 {
     [TestClass]
     public class CircularBufferTests
     {
-        private static Random rand;
+        private static Random _rand;
 
         public TestContext TestContext
         {
@@ -23,13 +22,13 @@ namespace CircularBuffer.Tests
         [ClassInitialize]
         public static void Initialize(TestContext context)
         {
-			rand = new Random();
+			_rand = new Random();
         }
 
 	    protected byte[] GenerateRandomBytes(int length)
 	    {
 			var bytes = new byte[length];
-		    rand.NextBytes(bytes);
+		    _rand.NextBytes(bytes);
 		    return bytes;
 	    }
 
@@ -40,6 +39,22 @@ namespace CircularBuffer.Tests
             Assert.IsTrue(buf.Capacity == 15);
         }
 
+	    [TestMethod]
+	    public void ConstructorFromCollectionTest()
+	    {
+		    ICollection<byte> collection = new LinkedList<byte>();
+			collection.Add(5);
+			collection.Add(209);
+			collection.Add(59);
+			collection.Add(61);
+
+		    var buff = new CircularBuffer<byte>(collection);
+		    foreach (var b in collection)
+		    {
+				Assert.IsTrue(buff.Contains(b));   
+		    }
+	    }
+
         [TestMethod]
         public void IterateBuffer()
         {
@@ -47,7 +62,7 @@ namespace CircularBuffer.Tests
 			buff.Put(new byte[] { 10, 20, 30 });
 	        buff.Get();
 			buff.Put(new byte[] { 90, 100 });
-	        var result = new byte[4] {20, 30, 90, 100};
+	        var result = new byte[] {20, 30, 90, 100};
 	        var i = 0;
 	        foreach (var b in buff)
 	        {
@@ -73,7 +88,7 @@ namespace CircularBuffer.Tests
 			var data = GenerateRandomBytes(count);
 			var buff = new CircularBuffer<byte>(100);
 		    buff.Put(data);
-			var lastData = new byte[2] { 20, 30 };
+			var lastData = new byte[] { 20, 30 };
 			buff.Put(lastData);
 			buff.Skip(count);
 		    CollectionAssert.AreEqual(buff.Get(2), lastData);
@@ -98,7 +113,7 @@ namespace CircularBuffer.Tests
 		[TestMethod]
 	    public void ContainsTest()
 	    {
-		    var data = new byte[5] { 10, 20, 0, 15, 20 };
+		    var data = new byte[] { 10, 20, 0, 15, 20 };
 			var buff = new CircularBuffer<byte>(10);
 			buff.Put(data);
 		    Assert.IsTrue(((ICollection<byte>) buff).Contains(20));
@@ -112,7 +127,7 @@ namespace CircularBuffer.Tests
 	    [TestMethod]
 	    public void AtIndexTest()
 	    {
-		    var data = new byte[5] {10, 20, 30, 40, 50};
+		    var data = new byte[] {10, 20, 30, 40, 50};
 			var buff = new CircularBuffer<byte>(10);
 			buff.Put(data);
 			Assert.AreEqual(10, buff[0]);
@@ -125,7 +140,7 @@ namespace CircularBuffer.Tests
 	    [TestMethod, ExpectedException(typeof (ArgumentOutOfRangeException))]
 	    public void AtIndexOverFlow()
 	    {
-			var data = new byte[5] { 10, 20, 30, 40, 50 };
+			var data = new byte[] { 10, 20, 30, 40, 50 };
 			var buff = new CircularBuffer<byte>(10);
 			buff.Put(data);
 			Assert.AreEqual(50, buff[4]);
